220290d893ea0f6505eb422af0a9da1b2909424a
Wassim <wbenguerine@gmail.com>
2023-07-27 20:30:59 +0200

    statitics cleaned and transformed

--- data_transformation.py
+++ data_transformation.py
@@ -28,5 +28,22 @@ def all_statistics_data_cleaner(id_value):
     # Rename the columns
     df.columns = ['Statistic', 'Home Team', 'Away Team']
 
-    df.to_csv(f'Transformed/all_statistics_data_{id_value}.csv', index=False)
+    # Resetting index to 'statistic'
+    df.set_index('Statistic', inplace=True)
+
+    # Flatten the dataframe into a single row
+    single_row = df.T.values.flatten()
+
+    # Create new dataframe with single row
+    df_single_row = pd.DataFrame(single_row).T
+
+    # Creating new meaningful column names
+    new_columns = [f"{team}_{stat}" for stat in df.index for team in ['Home', 'Away']]
+
+    # Assign the new column names to the single row dataframe
+    df_single_row.columns = new_columns
+
+    # Save the new dataframe to a CSV file
+    df_single_row.to_csv(f'Transformed/all_statistics_data_{id_value}.csv', index=False)
+
 all_statistics_data_cleaner(10833986)
