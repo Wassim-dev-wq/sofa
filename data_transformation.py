33af1fcd06131cdbfef2c915f910135884ca6cb6
Wassim <wbenguerine@gmail.com>
2023-07-27 20:18:08 +0200

    new

--- data_transformation.py
+++ data_transformation.py
@@ -1,70 +1,32 @@
-import numpy as np
 import pandas as pd
+import numpy as np
 
+def all_statistics_data_cleaner(id_value):
+    # Read the dataset
+    df = pd.read_csv(f'Cleaned Data/all_statistics_data_{id_value}.csv')
 
-# Function to clean percentages
-def clean_percentage(val):
-    try:
-        return float(val.strip('%')) / 100
-    except:
-        return None
+    # Remove any duplicates
+    df = df.drop_duplicates()
 
+    # Function to clean the data
+    # Function to clean the data
+    # Function to clean the data
+    def clean_data(value):
+        if '%' in value and '(' not in value:  # '50%'
+            return int(value.replace('%', ''))
+        elif '(' in value and '/' not in value:  # '228 (64%)'
+            return int(value.split(' ')[0])
+        elif '(' in value and '/' in value:  # '22/74 (30%)'
+            return int(value.split('(')[-1].replace('%)', ''))
+        else:  # '355'
+            return int(value)
 
-# Function to clean ratios
-def clean_ratio(val):
-    try:
-        num, denom = val.split('/')
-        return float(num) / float(denom)
-    except:
-        return None
+    # Apply the function to the 'home' and 'away' columns
+    df['home'] = df['home'].apply(lambda x: clean_data(x))
+    df['away'] = df['away'].apply(lambda x: clean_data(x))
 
+    # Rename the columns
+    df.columns = ['Statistic', 'Home Team', 'Away Team']
 
-# Function to clean count data
-def clean_count(val):
-    try:
-        return float(val)
-    except:
-        return None
-
-
-def all_statistics_transformer(id_value):
-    # Read CSV files
-    df_statistics = pd.read_csv(f'Cleaned Data/all_statistics_data_{id_value}.csv')
-
-    # insert 'match_id' at the first column (index 0)
-    #df_statistics.insert(0, 'match_id', 10833986)
-
-    # define the stats columns
-    stats_cols = ['name', 'home', 'away']
-
-    # make sure all rows have exactly 3 columns
-    df = df_statistics.dropna(subset=stats_cols)
-
-    # Count the number of stats for each match
-    stats_per_match = df_statistics[df_statistics['name'] == 'Ball possession'].shape[0]
-
-    # reshape the data
-    match_ids = np.repeat(np.arange(len(df) // stats_per_match), stats_per_match)
-    df['match_id'] = match_ids
-
-    # pivot the table
-    df_pivot = df.pivot(index='match_id', columns='name')
-
-    # flatten MultiIndex columns
-    df_pivot.columns = ['_'.join(col).rstrip('_') for col in df_pivot.columns.values]
-
-    # reset index
-    df_clean = df_pivot.reset_index(drop=True)
-
-    # handle erroneous data (replace non-numeric values with NaN)
-    for col in df_clean.columns:
-        df_clean[col] = pd.to_numeric(df_clean[col], errors='coerce')
-
-    # fill missing values
-    df_clean = df_clean.fillna(df_clean.median())
-
-    # save to csv
-    df_clean.to_csv('clean_match_data.csv', index=False)
-
-
-all_statistics_transformer(10833986)
+    df.to_csv(f'Transformed/all_statistics_data_{id_value}.csv', index=False)
+all_statistics_data_cleaner(10833986)
